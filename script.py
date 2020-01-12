f118a5bbdb0cad7b4a2a0aa15241d675c9af758d
jezdufourq <jezdufourq@gmail.com>
2020-01-12 18:07:07 +0100

    ADD: Updated the training code.

--- script.py
+++ script.py
@@ -1,27 +1,34 @@
+# -*- coding: utf-8 -*-
 """
-Editor de Spyder
+Created on Thu Jan  9 06:56:46 2020
 
-Este es un archivo temporal.
+@author: ASUS
 """
 
-# #%%
 # from IPython import get_ipython
 # def __reset__(): get_ipython().magic('reset -sf')
 
 # __reset__()
 
-# #Reset all variables cuz my IDE is dumb
+#-----------------------------------------------------------------------------#
 
-import math
 import numpy as np
 import keras.preprocessing
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Flatten
+from keras.layers import LSTM, MaxPooling1D, Conv1D
 
-#%%
-datafile_pos = 'train-pos.txt'
-datafile_neg = 'train-neg.txt'
+#-----------------------------------------------------------------------------#
+
+datafile_pos = 'data/train-pos.txt'
+datafile_neg = 'data/train-neg.txt'
+datafile_eval = 'data/evaluation.txt'
 
 dataset_pos = []
 dataset_neg = []
+dataset_eval = []
+dataset_eval_text = []
 
 data = []
 labels = []
@@ -32,54 +39,46 @@ y_train = []
 x_test = []
 y_test = []
 
-#Here we will store all the processed reviews
+vocab_size = 10000
+max_length = 100
+batch_size = 128
+
+#-----------------------------------------------------------------------------#
 
 def rev_split(datafile, dataset):
 
     with open(datafile, 'r') as f:
-        datalist = f.readlines()
-        c = ''
-        for i in datalist:
-            for j in i:
-                if j != '.':
-                    c = c + j
-                elif j == '.' and c != '':
-                    dataset.append(c)
-                    c = ''
-                else:
-                    pass
-                    
+        dataset = f.readlines()                    
     return dataset
 
-print('REV SPLIT FUNCTION')
-print(rev_split('data/train-pos.txt', dataset_pos))
-#Here we split the massive chaotic entropic messy review file into individual reviews
-
-def char_filter(dataset):
+def one_hot(dataset, vocab_size):
+    
+    onehotset = []
     
     for i in dataset:
-        index = dataset.index(i)
-        dataset[index] = keras.preprocessing.text.text_to_word_sequence(dataset[index], filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=False, split=' ')
-    return dataset
-print("CHAR_FILTER FUNCTION")
-print(char_filter(dataset_pos))
-#Here we filter all the unnecessary characters
-    
-def data_shuffle(data_pos, data_neg, data_shuffled, labels_shuffled, length):
+        onehotset.append(keras.preprocessing.text.one_hot(i, vocab_size, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' '))
+    return onehotset
+
+def data_shuffle(dataset_pos, dataset_neg, data_shuffled, labels_shuffled):
     
     i = 0
-    
-    while i < length:
-        data_shuffled.append(data_pos[i])
-        data_shuffled.append(data_neg[i])
+
+    while i < len(dataset_pos):
+        data_shuffled.append(dataset_pos[i])
+        data_shuffled.append(dataset_neg[i])
         labels_shuffled.append(1)
         labels_shuffled.append(0)
         i += 1
+        
+    i = 0
+    
     return data_shuffled, labels_shuffled
 
-def data_split(xtrain, ytrain, xtest, ytest, data, labels, length):
+
+def data_split(xtrain, ytrain, xtest, ytest, data, labels):
     
-    train_length = int(length * 0.8)
+    length = len(data)
+    train_length = int(len(data) * 1)
     i = 0
     
     while i < length:
@@ -91,51 +90,80 @@ def data_split(xtrain, ytrain, xtest, ytest, data, labels, length):
             xtest.append(data[i])
             ytest.append(labels[i])
             i += 1
+    
+    i = 0
+    length = 0
+    train_length = 0
             
     return xtrain, ytrain, xtest, ytest
 
+#-----------------------------------------------------------------------------#
+
+print("Uploading data...\n")
+
+#Splitting reviews
+dataset_pos = rev_split(datafile_pos, dataset_pos)
+dataset_neg = rev_split(datafile_neg, dataset_neg)
+dataset_eval = rev_split(datafile_eval, dataset_eval)
+dataset_eval_text = rev_split(datafile_eval, dataset_eval)
+
+print("Total reviews: "+str(len(dataset_pos)+len(dataset_neg)))
+print("Number of negative reviews: "+str(len(dataset_neg)))
+print("Number of positive reviews: "+str(len(dataset_pos)))
+print("Total test reviews: "+str(len(dataset_eval)))
+
+
+print("\nPreprocessing...")
+
+#One-hot-encoding reviews
+dataset_pos = one_hot(dataset_pos, vocab_size)
+dataset_neg = one_hot(dataset_neg, vocab_size)
+dataset_eval = one_hot(dataset_eval, vocab_size)
+
+#Shuffling data
+data_shuffle(dataset_pos, dataset_neg, data, labels)
+
+#Splitting preliminary train and test data
+data_split(x_train, y_train, x_test, y_test, data, labels)
+
+#Padding
+x_train = sequence.pad_sequences(x_train, maxlen=max_length, padding = 'post')
+x_test = sequence.pad_sequences(x_test, maxlen=max_length, padding = 'post')
+dataset_eval = sequence.pad_sequences(dataset_eval, maxlen=max_length, padding = 'post')
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+
+#ML Model
+print('\nBuilding model...')
+
+model = Sequential()
+model.add(Embedding(vocab_size, 128, input_length = max_length))
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+    
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+print(model.summary())
+
+print('Training...')
+model.fit(x_train, y_train,
+          batch_size = batch_size,
+          epochs = 5,
+          shuffle = True)
+          #validation_data = (x_test, y_test))
+#loss, acc = model.evaluate(x_test, y_test,
+#                            batch_size=batch_size)
+#print('Test accuracy:', acc)
+
+classes = model.predict(dataset_eval, batch_size = batch_size)
+print(classes)
 #%%
-def main_func():
-    max_length = 100
-    #%%
-    print("Uploading data...")
-
-    rev_split(datafile_pos, dataset_pos)
-    rev_split(datafile_neg, dataset_neg)
-
-    #%%
-    print("Preprocessing...")
-
-    char_filter(dataset_pos)
-    char_filter(dataset_neg)
-
-    #%%
-    print("Shuffling...")
-
-    data_shuffle(dataset_pos, dataset_neg, data, labels, max_length)
-
-    #%%
-    print("Splitting...")
-
-    data_split(x_train, y_train, x_test, y_test, data, labels, max_length)
-    print("---------- TRAIN DATA ----------\n\n")
-
-    i = 0
-
-    while i < len(x_train):
-        print(str(i)+"\t"+str(x_train[i])+"\t"+str(y_train[i]))
-        i += 1
-        
-    i = 0
-
-    print("---------- TEST DATA ----------\n\n")
-
-    i = 0
-
-    while i < len(x_test):
-        print(str(i)+"\t"+str(x_test[i])+"\t"+str(y_test[i]))
-        i += 1
-        
-
-
-
+print(len(classes))
+print(len(dataset_eval))
+for i in range(len(dataset_eval)):
+    print(str(dataset_eval_text[i])+"\t"+str(classes[i])+"\n")
